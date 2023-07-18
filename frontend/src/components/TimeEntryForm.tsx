d5f8b06b6287477240f502a9a3c6d3601054dd84
MetaSeth <alexandre.mokni@gmail.com>
2023-07-18 22:32:26 +0200

    added project and task selectors + userId implicitly in timeEntryForm

--- frontend/src/components/TimeEntryForm.tsx
+++ frontend/src/components/TimeEntryForm.tsx
@@ -1,18 +1,39 @@
-import { Form, Input, Button, DatePicker, InputNumber } from 'antd'
+import { Form, Input, Button, DatePicker, InputNumber, Select } from 'antd'
 import { addTimeEntry } from '../services/TimeEntryService'
 import { firestore } from '../firebase'
 import { Timestamp } from 'firebase/firestore'
+import { useContext, useEffect, useState } from 'react'
+import { getTasks } from '../services/TaskService'
+import { getProjects } from '../services/ProjectService'
+import UserContext from '../context/UserContext'
+import Project from '../interfaces/Project'
+import Task from '../interfaces/Task'
 
-// TODO: add Project , task and user selectors
 const TimeEntryForm = () => {
     const [form] = Form.useForm()
+    const userContext = useContext(UserContext)
+    const currentUser = userContext?.user || null
+    const [tasks, setTasks] = useState<Task[]>([])
+    const [projects, setProjects] = useState<Project[]>([])
+    const projectValue = Form.useWatch('projectId', form)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const fetchedTasks = await getTasks(firestore)
+            const fetchedProjects = await getProjects(firestore)
+
+            setTasks(fetchedTasks)
+            setProjects(fetchedProjects)
+        }
+        fetchData()
+    }, [firestore, currentUser, tasks, projects])
 
     const onFinish = async (values: any) => {
         console.log('Received values of form: ', values)
-
         const formattedValues = {
             ...values,
             date: Timestamp.fromDate(values.date.toDate()),
+            userId: currentUser?.id,
         }
         console.log('formattedValues:', formattedValues)
 
@@ -20,37 +41,45 @@ const TimeEntryForm = () => {
         form.resetFields()
     }
 
+    const renderTasksofProject = () => {
+        const projectTasks = tasks.filter(
+            (task) => task.projectId === projectValue
+        )
+        return projectTasks.map((task) => (
+            <Select.Option key={task.id} value={task.id}>
+                {task.name}
+            </Select.Option>
+        ))
+    }
+
     return (
         <Form form={form} onFinish={onFinish} layout="vertical">
             <Form.Item
-                label="Project ID"
+                label="Project"
                 name="projectId"
                 rules={[
                     { required: true, message: 'Please enter the project ID' },
                 ]}
             >
-                <Input placeholder="Project ID" />
+                <Select>
+                    {projects.map((project) => (
+                        <Select.Option key={project.id} value={project.id}>
+                            {project.name}
+                            {project.id}
+                        </Select.Option>
+                    ))}
+                </Select>
             </Form.Item>
             <Form.Item
-                label="Task ID"
+                label="Task"
                 name="taskId"
+                hidden={!projectValue}
                 rules={[
                     { required: true, message: 'Please enter the task ID' },
                 ]}
             >
-                <Input placeholder="Task ID" />
+                <Select>{renderTasksofProject()}</Select>
             </Form.Item>
-
-            <Form.Item
-                label="User ID"
-                name="userId"
-                rules={[
-                    { required: true, message: 'Please enter the user ID' },
-                ]}
-            >
-                <Input placeholder="User ID" />
-            </Form.Item>
-
             <Form.Item
                 label="Date"
                 name="date"
