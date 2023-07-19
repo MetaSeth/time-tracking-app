f2a9ce6405ddc2b8fa1c545cc8bf6893cb55d5f8
MetaSeth <alexandre.mokni@gmail.com>
2023-07-19 06:20:50 +0200

    more clear

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
             <div
                 style={{
                     display: 'flex',
-                    width: '100%',
+                    width: '75%',
                     justifyContent: 'end',
                 }}
             >
